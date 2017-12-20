04af7af1066800f9d50ed4289b3e9a55ec8010fa
turnbot <turnbot@gmail.com>
2017-12-20 08:05:44 -0800

    Inital Commit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,87 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "semantic-ui-css/semantic.min.css";
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import rootReducer from "./rootReducer";
+import React from 'react';
+import PropTypes from "prop-types";
+import { Form, Button, Message } from 'semantic-ui-react';
+import Validator from 'validator';
+import InlineError from './components/messages/InlineError';
 
+class LoginForm extends React.Component {
+        state = {
+            data: {
+                email: '',
+                password: ''
+            },
+            loading: false,
+            errors: {}
+        }
+        onChange = e =>
+            this.setState({
+                data: {...this.state.data, [e.target.name]: e.target.value }
+            });
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+        onSubmit = () => {
+            const errors = this.validate(this.state.data);
+            this.setState({ errors });
+            if (Object.keys(errors).length === 0) {
+                this.setState({ loading: true });
+                this.props
+                    .submit(this.state.data)
+                    .catch(err => this.setState({ errors: err.response.data.errors, loading: false }));
+            }
+        }
 
-ReactDOM.render(
-	<BrowserRouter>
-	<Provider store={store}><App /></Provider>
-	</BrowserRouter>,
-	document.getElementById("root")
-);
-registerServiceWorker();
+        validate = (data) => {
+            const errors = {};
+            if (!Validator.isEmail(data.email)) errors.email = "Invalid Email";
+            if (!data.password) errors.password = "Cannot be Blank";
+
+            return errors;
+        }
+        render() {
+                const { data, errors, loading } = this.state;
+                return ( <
+                    Form onSubmit = { this.onSubmit }
+                    loading = { loading } > {
+                        errors.global && ( < Message negative >
+                            <
+                            Message.Header > Oops!Something is wrong. < /Message.Header> <
+                            p > { errors.global } < /p> <
+                            /Message>
+                        )
+                    } <
+                    Form.Field error = {!!errors.email } >
+                    <
+                    label htmlFor = "email" > Email < /label> <
+                    input type = "email"
+                    id = "email"
+                    name = "email"
+                    placeholder = "example@example.com"
+                    value = { data.email }
+                    onChange = { this.onChange }
+                    />  {
+                        errors.email && < InlineError text = { errors.email }
+                        />} <
+                        /Form.Field> <
+                        Form.Field error = {!!errors.password } >
+                            <
+                            input
+                        type = "password"
+                        id = "password"
+                        name = "password"
+                        placeholder = "Make it Secure"
+                        value = { data.password }
+                        onChange = { this.onChange }
+                        />  {
+                            errors.password && < InlineError text = { errors.password }
+                            />}  <
+                            /Form.Field> <
+                            Button primary > Login < /Button> <
+                                /Form>
+                        );
+                    }
+                }
+
+                LoginForm.propTypes = {
+                    submit: PropTypes.func.isRequired
+                };
+
+                export default LoginForm;
